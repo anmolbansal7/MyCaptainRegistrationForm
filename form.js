58c9f5bf6cffd75e9b3bf96cfabda8ab8720d55a
Anmol Bansal <abanmolbansal5@gmail.com>
2020-05-15 15:55:49 +0530

    Inital Commit

---
+++ form.js
@@ -0,0 +1,17 @@
+//JS
+//Client Side Object Oriented Programming Language
+//DOM elements
+//query selector
+
+
+function registerform()
+{
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var DOB = document.getElementById("DOB").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var male = document.getElementById("male").checked;
+    var female = document.getElementById("female").checked;
+    var subject = document.getElementById("subject").value;
+}
